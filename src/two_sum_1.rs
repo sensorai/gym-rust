107bb48a28f5c161d50e78992e85cf2287b9d508
Austin Chen <austin.yhc@gmail.com>
2022-08-01 15:08:52 +0800

    :test_tube: Add a test item

--- src/two_sum_1.rs
+++ src/two_sum_1.rs
@@ -36,6 +36,7 @@ impl Solution {
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,6 +45,7 @@ mod tests {
     fn test_1() {
         assert_eq!(vec![0,1], Solution::two_sum(vec![2,7,11,15], 9));
         assert_eq!(vec![1,2], Solution::two_sum(vec![3,2,4], 6));
+        assert!(Solution::two_sum(vec![2,7,11,15], 19).is_empty());
     }
 }
 
